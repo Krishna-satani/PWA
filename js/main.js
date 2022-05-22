3397e277d1fe1e3c61ef5d1b663e49b38cf4665a
Krishna satani <krishnasatani@Krishnas-MacBook-Pro.local>
2022-05-21 20:41:11 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js', { scope: '/' })
+        .then((res) => {
+            console.log(res)
+        })
+        .catch(err => console.log(err));
+} else {
+    console.log('Service worker is not supported.')
+}
