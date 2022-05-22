b5202a21bb6f0026b40fe9b2fd180b58f941bf3a
krishna <kroshnasatani2000@gmail.com>
2022-05-21 21:02:54 -0400

    fixed service-worker

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js', { scope: '/' })
+    navigator.serviceWorker.register('/PWA/service-worker.js', { scope: '/PWA/' })
         .then((res) => {
             console.log(res)
         })
