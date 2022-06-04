b7c35d7c77d29cf59d19499be0f8f6b1f83f6e5c
krishna <kroshnasatani2000@gmail.com>
2022-06-04 17:12:56 -0400

    update PWA

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,49 @@
+
+const cachename = "lab02"
+
 self.addEventListener('fetch', function (event) {
     console.log('Fetch', event)
+    event.respondWith(
+        caches.open(cachename).then(res =>{
+            return res.match(event.request).then(storeres => {
+                const fetchdata = fetch(event.request).then(fetch_response => {
+                    res.put(event.request,fetch_response)
+                    
+                    return fetch_response
+
+                })
+                return storeres || fetchdata
+            })
+        })
+    )
+});
+
+self.addEventListener('install', function (event) {
+    console.log('install', event)
+    self.skipWaiting()
+    event.waitUntil(
+        caches.open(cachename).then(res => {res.addAll([
+            "/PWA/",
+            "/PWA/index.html",
+            "/PWA/js/main.js",
+            "/PWA/css/style.css",
+            "/PWA/manifest.json",
+            "/PWA/icons/favicon-196.png",
+            "/PWA/images/loginlogo.png"
+        ])})
+    
+    )
+    
+});
+
+
+self.addEventListener('activate', function (event) {
+    console.log('activate', event)
+
+    event.waitUntil(clients.claim())
+    event.waitUntil(caches.keys().then(ch => {
+        return Promise.all(ch.filter(item => item != cachename).map(data => caches.delete(data)))
+
+    }))
+    
 });
